3133abff0f51400d8c1d2f4a9bc5899fef14c519
pearl0nyak <pearlnyax@gmail.com>
2024-05-06 00:18:54 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Scholar on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
